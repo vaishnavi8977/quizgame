2772eb9c5e976899c803cc4e9ef19be75ebc47c3
vaishnavi8977 <vdade@wildcats.unh.edu>
2022-05-16 12:34:20 -0400

    declaring show_question()

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ class Context(GameContext):
     def __init__(self):
         self.game = Game(self)
 
+    def show_question(self, question):
+        """Displays the question to a user."""
+        answer = input(question).upper()
+        if(answer == "YES"):
+            # self.game.answer_yes("You answered YES. Continue?")
+            self.game.answerYes.answer_yes(question)
+        elif(answer == "NO"):
+            self.game.answerNo.answer_no(question)
+        else:
+            print("Unknown input " + answer)
+
 
 def main():
     context = Context()
