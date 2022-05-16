ed3db7f33d043cd749b0cd93ac296897f357ceaa
vaishnavi8977 <vdade@wildcats.unh.edu>
2022-05-16 12:28:18 -0400

    define sub class AswerYes and logic for answer_yes

--- game.py
+++ game.py
@@ -25,3 +25,24 @@ class Game:
         #                            "Think of a design pattern and answer these following yes/no questions. "
         #                            "Ready?")
             self.context.show_question(self.context.questions[0]["question"])
+
+
+    class AnswerYes:
+        def __init__(self, context: GameContext):
+            self.context = context
+
+        def answer_yes(self, question):
+            """It's called when a user answers YES to a question.
+         Handles the transition to the next question or exits the game.
+         """
+
+            for x in self.context.questions:
+                if x["question"] == question:
+                    next_question_id = int(x["yes"])
+                    if next_question_id == 0:
+                        self.context.end_game()
+                    else:
+                        for x in self.context.questions:
+                            if x["id"] == next_question_id:
+                                next_question = x["question"]
+                                self.context.show_question(next_question)
